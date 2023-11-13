da31528ec90a120fb00c90a6b8beddb550d1c80b
sylay1 <sylaypaper@gmail.com>
2023-11-13 20:44:31 +0100

    blender version now updates the connection status

--- blenderKritaLink/__init__.py
+++ blenderKritaLink/__init__.py
@@ -1,12 +1,13 @@
 from multiprocessing.connection import Listener,Client
 import time
 from random import random
-from threading import Thread
+from threading import Thread, Timer
 from multiprocessing import shared_memory
 import struct
 import array
 import bpy
 import numpy as np
+from .ui  import BlenderKritaLinkPanel
 from .image_manager import ImageManager
 from .connection import KritaConnection 
 
@@ -21,22 +22,10 @@ bl_info = {
 #t1.start()
 T1 = None
 
-class BlenderKritaLinkPanel(bpy.types.Panel):
-    """Blender Krita Link Panel"""
-    bl_label = "Krita Link"
-    bl_space_type = 'IMAGE_EDITOR'
-    bl_region_type = 'UI'
-    bl_category = 'Blender Krita Link'
-
-    def draw(self, context):
-        layout = self.layout
-        row = layout.row()
-        row.label(text="connection: " + KritaConnection.STATUS)
-        col = layout.column()
-        for image in bpy.data.images:
-            col.label(text=image.name)
-        col = layout.column()
-        col.operator(HelloWorldOperator.bl_idname,text="say hello",icon = "UV_FACESEL")
+def update_gui():
+    print("siema")
+    for area in bpy.context.screen.areas:area.tag_redraw()
+    print("elo")
 
         
         
@@ -51,27 +40,9 @@ class HelloWorldOperator(bpy.types.Operator):
     def poll(cls, context):
         return False
     
-class LoadImage(bpy.types.Operator):
-    bl_idname = "wm.hello_world"
-    bl_label = "Minimal Operator"
-
-    def execute(self, context):
-        print("Hello World")
-        return {'FINISHED'}
-    @classmethod
-    def poll(cls, context):
-        return False
-
-def siema():
-    print("siema")
-
-
 def register():
     bpy.utils.register_class(HelloWorldOperator)
     bpy.utils.register_class(BlenderKritaLinkPanel)
-    # image_name = "Untitled"
-    # image_src = bpy.data.images[image_name]
-    # src_len = len(image_src.pixels)
 
 
 def unregister():
@@ -79,10 +50,13 @@ def unregister():
     image_manager_instance = None
     bpy.utils.unregister_class(HelloWorldOperator)
     bpy.utils.unregister_class(BlenderKritaLinkPanel)
+    # del bpy.types.Scene.connection_status
+    del bpy.types.Scene.test_prop
 
 if __name__ == "__main__":
     register()
 
 connection_instance = KritaConnection()
+print(dir(connection_instance))
+connection_instance.start()
 image_manager_instance = ImageManager()
-    
--- blenderKritaLink/connection.py
+++ blenderKritaLink/connection.py
@@ -5,11 +5,10 @@ from threading import Thread,Event
 from multiprocessing import shared_memory
 import struct
 import array
-
-from click import launch
 import bpy
 import numpy as np
 from .image_manager import ImageManager
+from .ui import BlenderKritaLinkPanel
 
 class KritaConnection():
     PORT = 6000
@@ -19,27 +18,33 @@ class KritaConnection():
     def __init__(self) -> None:
         if KritaConnection.LINK_INSTANCE: return
         KritaConnection.LINK_INSTANCE = self
-        self.__THREAD = Thread(target=self.krita_listener)
-        self.__THREAD.start()
-        self.__STOP_SIGNAL = Event()
-        self.CONNECTION: None|Connection = None
-        KritaConnection.STATUS = 'listening'
 
     def __del__(self):
-        self.__STOP_SIGNAL.set()
         if self.CONNECTION:
             self.CONNECTION.send("close")
             self.CONNECTION.close()
-            
+    
+    def start(self):
+        self.__STOP_SIGNAL = Event()
+        self.__THREAD = Thread(target=self.krita_listener)
+        self.__THREAD.start()
+        self.CONNECTION: None|Connection = None
+        KritaConnection.STATUS = 'listening'
+        
+    def update_message(self,message):
+        if hasattr(bpy.context,'scene'):
+            bpy.context.scene.test_prop= "listening"
+        else: print("no scene??")
+        
     def krita_listener(self):
         """chuj"""
-        while True:
+        while not self.__STOP_SIGNAL.isSet():
             KritaConnection.LINK_INSTANCE = self
             address = ('localhost', KritaConnection.PORT)     # family is deduced to be 'AF_INET'
-            KritaConnection.STATUS = "listening"
+            self.update_message("listening")
             listener = Listener(address, authkey=b'2137')
             conn = listener.accept()
-            KritaConnection.STATUS = "connected"
+            self.update_message("connected")
             KritaConnection.CONNECTION = conn
             print("connection accepted")
             existing_shm = shared_memory.SharedMemory(name='krita-blender')
@@ -50,7 +55,6 @@ class KritaConnection():
                     if msg == 'close':
                         print(msg)
                         conn.close()
-                        existing_shm.close()
                         break
                     elif msg == 'refresh':
                         t = time.time()
@@ -58,6 +62,7 @@ class KritaConnection():
                         fp32_array = np.frombuffer(existing_shm.buf, dtype=np.float32)
                         print("refresh initiated")
                         ImageManager.INSTANCE.mirror_image(fp32_array)
+                        fp32_array = None
                         print("refresh complete")
                     # elif isinstance(msg, bytes):
                     #     ImageManager.update_image(msg)
@@ -70,7 +75,9 @@ class KritaConnection():
                     self.CONNECTION.send("close")
                     self.CONNECTION.close()
                 self.CONNECTION = None
-                existing_shm.close()
+            # existing_shm.close()
             listener.close()
             if self.__STOP_SIGNAL.is_set():
-                break
+                KritaConnection.STATUS = "listening"
+                self.redraw_uis()            
+                return
