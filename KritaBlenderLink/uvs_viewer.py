15f9191165a3b8955ef70ece069273a1e4aefb43
wyvernbw <iamwyvern511@gmail.com>
2026-09-08 21:01:13 +0300

    fix zoom calculation

--- KritaBlenderLink/uvs_viewer.py
+++ KritaBlenderLink/uvs_viewer.py
@@ -62,7 +62,7 @@ def get_transform(view):
         print("view is none")
         return QTransform()
 
-    zoom = (canvas.zoomLevel() * 72.0) / document.resolution()
+    zoom = canvas.zoomLevel()
     transform = QTransform()
 
     transform.translate(view.flakeToCanvasTransform().dx(),view.flakeToCanvasTransform().dy())
